39b1e37b42c8b3074b555f3ef2ff5d9feceeba5d
haiz usman <gourgeousm@gmail.com>
2022-06-03 08:10:14 -0700

    js code

---
+++ App.js
@@ -0,0 +1,265 @@
+// var a = "mani"
+
+// var b = 125
+
+// var c = "125"
+
+// console.log (a+b+c)
+
+
+// alert ("hello world!");
+
+// var a = 5
+// var b = 10
+
+// alert (a+b)
+// alert (b-a)
+// alert (a*b)
+
+
+// var num = 7
+// var num2 = 10
+
+// var add = num+num2
+
+// var sub = num2-num
+
+// var multi = num1*num2
+
+// var divide = num2/num1
+
+// alert ("this is addition" + " " + add)
+// alert ("this is subtraxtion" + " " + sub)
+// alert ("this is Multipliccation" + " " + multi)
+// alert ("this is division" + " " + divide)
+
+// var mix = num+3 + num2 - num1 + (num*num1)
+
+//             10 + 10 - 5 + 35//
+
+// alert (mix)            
+
+
+// var num1 = 5
+
+// var a = num1++ + num1++ + ++num1 - num1 + --num1 - num1-- + num1++ - num1-- - ++num1 + num1
+
+// //       5     +    6   +    8   -  8   +    7    -  7    +   6    -   7   -     7    +  7
+
+// alert (a)
+
+
+
+// var n = 2
+
+// var total = n++ + n-- + n + --n - --n + n++ + ++n + --n - n++ - n + n - ++n - --n
+
+//         //  2   +  3  + 2 +  1  -  0  +  0  +  2  +  1  -  1  - 2 + 2 -  3  -  2
+
+// alert (total)
+
+
+
+
+// var user = +prompt("enter your key")
+
+// if (user !== 8877){
+
+//     alert ("welcome to your dashboard")
+
+// }
+// else {
+//     alert ("invalid key")
+// }
+
+// var country = prompt("enter your country name")
+
+// var province = prompt("Enter your province")
+
+// // var education = ("Education")
+
+// var weight = +prompt("enter your weight")
+
+// var gender = prompt("enter your gender")
+
+// if (country == "pakistan" && province == "sindh" && gender == "male" && weight >= 60 && weight <= 70 ){
+
+//     alert ("you are allowed to ride")
+// }
+// else {
+//     alert ("you are not allowed to ride")
+// }
+
+
+
+// var age = +prompt("enter your age")
+
+// var weight = prompt("enter your weight")
+
+// var country = prompt("enter your country")
+
+// var city = prompt("Enter your city")
+
+// if (age >= 20 && weight >= 50 && (country == "pakistan" || city == "karachi")){
+
+//     alert ("you are allowed to ride")
+
+// }
+// else {
+//     alert ("you are not allowed to ride")
+// }
+
+
+
+
+// var name1 = "amir"
+// var name2 = "kashif"
+// var name3 = "bilal"
+// var name4 = "zubair"
+// var name5 = "imran"
+
+// console.log (name1)
+
+
+// var arr = ["amir","imran","kashif","ali","pakistan",3342849423]
+
+// arr.pop()
+// // arr.push(03342978864,03353038265,"kasmflakmf","dksfkl","kljnscvfkl","dksmfjs")
+
+// // arr.shift()
+
+// arr.unshift("mani","hasan")
+
+
+// // arr.splice(1,4)
+
+// var arr1 = arr.slice(2,5)
+
+//  console.log (arr)
+//  console.log (arr1[1])
+
+// for (var i = 0; i <= 10; i++){
+//     if (i === 5){
+        
+//         // break
+        
+//     }
+//     alert ("mani 5 dafa")
+//     // alert ("hello world")
+// }
+
+// var a = prompt("enter your value")
+
+// var arr = ["mani","adeel","usman","imran"]
+
+// for (var i = 0; i <= arr.length; i++){
+//     if (a === "usman"){
+//         alert ("same person")
+//         break
+//     }else if (a === "imran"){
+//         alert ("brother")
+//         break
+//     }else if (a === "adeel"){
+//         alert ("Friend")
+//         break
+//     }else {
+//         alert("not a correct value")
+//         break
+//     }
+// }
+
+
+// for (var i = 0; i < 10; i++){
+//     for (var j = 0; j < 5; j++){
+
+//         console.log ("hello world")
+        
+
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
